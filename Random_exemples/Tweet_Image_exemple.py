c59a9e1f76ce9ed7ff312ccd00cd041ba6485ffa
Thibault <thibault.mathian@free.fr>
2020-04-19 12:26:03 +0200

    add tweet image exemple

---
+++ Random_exemples/Tweet_Image_exemple.py
@@ -0,0 +1,35 @@
+### via https://gist.github.com/szolotykh/e4924159d79ddbaa12c6
+
+
+import tweepy
+import os
+
+# Consumer keys and access tokens, used for OAuth
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+
+# Creates the user object. The me() method returns the user whose authentication keys were used.
+user = api.me()
+
+print('Name: ' + user.name)
+print('Location: ' + user.location)
+print('Friends: ' + str(user.friends_count))
+
+# Sample method, used to update a status
+# api.update_status('Hello Form RBI Lab!')
+
+# load image
+imagePath = "img.jpg"
+status = "Hi! From Python script=)"
+
+# Send the tweet.
+api.update_with_media(imagePath, status)
